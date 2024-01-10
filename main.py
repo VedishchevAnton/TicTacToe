95bcc80a944a25f7a27e5f5b4638e10ecebd3518
VedishchevAnton <vedishchevanton@gmail.com>
2024-01-10 23:19:42 +0300

    реализация инициатора class TicTacToe

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ class Cell:
     def __bool__(self):
         return self._value == 0  # bool(cell) - возвращает True,
         # если клетка свободна (value = 0) и False - в противном случае.
+
+
+class TicTacToe:
+
+    def __init__(self):
+        self._size = 3
+        self._win = 0  # 0 - игра, 1 - победа человека, 2 - победа компьютера, 3 - ничья
+        self.pole = tuple(
+            tuple(Cell() for _ in range(self._size)) for _ in range(self._size))  # двумерный кортеж, размером 3x3.
+
+
+
+
