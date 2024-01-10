3fb292a66f89c09e5169a593bef7c732b907d091
VedishchevAnton <vedishchevanton@gmail.com>
2024-01-10 23:43:45 +0300

    реализация магических методов getitem и setitem, а также функции проверки статуса игры

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from random import randint
 
 class Cell:
     def __init__(self):
-        self._value = 0  # value - текущее значение в ячейке: 0 - клетка свободна; 1 - стоит крестик; 2 - стоит нолик.
+        self.value = 0  # value - текущее значение в ячейке: 0 - клетка свободна; 1 - стоит крестик; 2 - стоит нолик.
 
     def __bool__(self):
-        return self._value == 0  # bool(cell) - возвращает True,
+        return self.value == 0  # bool(cell) - возвращает True,
         # если клетка свободна (value = 0) и False - в противном случае.
 
 
 class TicTacToe:
+    FREE_CELL = 0  # свободная клетка
+    HUMAN_X = 1  # крестик (игрок - человек)
+    COMPUTER_O = 2  # нолик (игрок - компьютер)
 
     def __init__(self):
         self._size = 3
@@ -25,11 +28,52 @@ class TicTacToe:
          """
         if type(index) not in (tuple, list) or len(index) != 2:
             raise IndexError('некорректно указанные индексы')
-        r, c = index
+        r, c = index  # распаковка
         if not (0 <= r < self._size) or (0 <= c < self._size):
             raise IndexError('некорректно указанные индексы')
 
+    def __update_win_status(self):
+        """Функция проверки статуса игры"""
+        for row in self.pole:  # проверка в строках
+            if all(x.value == self.HUMAN_X for x in row):
+                self._win = 1
+                return
+            if all(x.value == self.COMPUTER_O for x in row):
+                self._win = 2
+                return
 
+        for i in range(self._size):  # проверка в столбцах
+            if all(x.value == self.HUMAN_X for x in (row[i] for row in self.pole)):
+                self._win = 1
+                return
 
+            if all(x.value == self.COMPUTER_O for x in (row[i] for row in self.pole)):
+                self._win = 2
+                return
 
+        # проверка в диагоналях
+        if all(self.pole[i][i].value == self.HUMAN_X for i in range(self._size)) or \
+                all(self.pole[i][-1 - i].value == self.HUMAN_X for i in range(self._size)):
+            self._win = 1
+            return
 
+        if all(self.pole[i][i].value == self.COMPUTER_O for i in range(self._size)) or \
+                all(self.pole[i][-1 - i].value == self.COMPUTER_O for i in range(self._size)):
+            self._win = 2
+            return
+
+        # проверка ничьи
+        if all(x.value != self.FREE_CELL for row in self.pole for x in row):
+            self._win = 3
+            return
+
+    def __getitem__(self, item):
+        self.__check_index(item)
+        r, c = item  # распаковка
+        return self.pole[r][c].value
+
+    def __setitem__(self, key, value):
+        self.__check_index(key)
+        r, c = key
+        self.pole[r][c].value = value
+        self.__update_win_status()
